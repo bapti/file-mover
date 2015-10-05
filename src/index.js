25968dd7aba52108f89fcd1c1dfaef3873de87d5
Neil Crawford <neil.crawford@gmail.com>
2015-10-05 21:14:37 +0100

    Working now

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ var fs = require("fs")
 var fsExtra = require("fs-extra")
 var globby = require("globby")
 var path = require("path")
-var del  =  require("del")
+var del = require("del")
+var rm = require("rimraf")
 
+process.on('uncaughtException', function(err) {
+  console.log(err);
+});
 
 var moveFile = _.wrapCallback(
   (oldPath, done) => {
     fileName = path.basename(oldPath)
     newPath = `C:/Users/Neil/torrents/${fileName}`
     console.log(`
-Moving file: ${fileName}
-  from : ${oldPath}
-  to :   ${newPath}
-`)
+      Moving file: ${fileName}
+        from : ${oldPath}
+        to :   ${newPath}
+      `)
     fs.rename(oldPath, newPath, done)
   }
 )
+
+var deleteFile = _.wrapCallback(rm)
 
 var getFilesizeInMegaBytes = (filename) => {
   var stats = fs.statSync(filename)
@@ -31,13 +37,11 @@ var cleaner = _(globby("C:/Users/Neil/torrents/*/"))
   .filter((filePath) => {
     return !filePath.endsWith("Temp/")
   })
-  .map(_.wrapCallback(
-    (filePath, done) =>{
-      del([filePath], (paths) => {
-        done(null, paths)
-      })
-    }
-  ))
+  .map(deleteFile)
+  .sequence()
+  .errors((err) => {
+    console.log(err);
+  })
 
 var errors = []
 
@@ -53,18 +57,13 @@ _(globby("C:/Users/Neil/torrents/*/*", '!C:/Users/Neil/torrents/Temp**'))
   .map(moveFile)
   .parallel(10)
   .errors((err) => {
-    if(err){
-      errors.push(err)
-    }
+    console.log(err);
+    errors.push(err)
   })
   .toArray((results) =>{
     if(errors.length > 0 ) {
       throw new Error("Arg something went wrong :(")
     }
 
-    cleaner.toArray((results) => {
-      results.forEach((item) =>{
-        console.log(`deleted file ${item}`);
-      })
-    })
+    cleaner.toArray((results) => {})
   })
